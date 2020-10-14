8e998734aafa6248d0e3d664574516c5673b20e7
nabzoouu <nabilzine.nz@gmail.com>
2020-10-14 21:53:28 +0000

    Séparation des fichiers routes, cli_commands et databases pour qu'il n'y ai qu'un objet par fichier et création des fichiers index pour tous les importer facilement dans app.py Création d'un fichier crontab pour mettre en place la récupération des new release daily Création des fichiers de config Mis à jour de la route pour qu'elle afficher également les nouveaux albums liés au artistes

--- server/app.py
+++ server/app.py
@@ -4,28 +4,30 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 from sqlalchemy import Table, Column, Integer, ForeignKey
 
-from spotify_client_model import build_spotify_client
-from models.database import build_db
-from routes import build_routes
+from models.index import build_db
+from routes.index import build_routes
+from cli_command.init_db_test import build_init_test_db_command
+from cli_command.drop_db import build_drop_db_cli_command
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
+if app.config["ENV"] == "production":
+  app.config.from_object("config.ProductionConfig")
+else:
+  app.config.from_object("config.DevelopmentConfig")
+app.config.from_pyfile("secret/credentials.py")
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@172.17.0.2:5432/new-release'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{app.config['TYPE_OF_DATABASE']}://{app.config['POSTGRES_USER']}:{app.config['POSTGRES_PASSWORD']}@{app.config['DATABASE_HOST']}:{app.config['DATABASE_PORT']}/{app.config['DATABASE_NAME']}"
 
 db = SQLAlchemy(app)
+(Album, Artist, Album_artist) = build_db(db)
 
-Artist, Album, Album_artist = build_db(db)
-db.drop_all()
-db.create_all()
+build_routes(app, db, Album = Album, Artist = Artist)
 
-spotifyClient = build_spotify_client(db, app.config['CLIENT_ID'], app.config['CLIENT_SECRET'], Album=Album, Artist=Artist, Album_artist=Album_artist)
-
-build_routes(app, db, spotifyClient, Album = Album, Artist = Artist)
-
-print(spotifyClient.storeNewRelease({}))
+if app.config["ENV"] != "production":
+  build_drop_db_cli_command(db, app)
+  build_init_test_db_command(app, db, Album = Album, Artist = Artist)
 
 if __name__ == "__main__":
-  app.run(debug=True, host='localhost', port='3000')
+  app.run(debug=app.config["DEBUG"], host=app.config["APP_HOST"], port=app.config["APP_PORT"])
